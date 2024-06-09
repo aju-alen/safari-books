f6b6f24a16d6e3181618c9e9415739ac509d896c
Alen <alenaju1304@gmail.com>
2024-06-09 22:03:26 +0400

    added url

--- api/utils/backendUrl.js
+++ api/utils/backendUrl.js
@@ -1,1 +1,1 @@
-export const backendUrl = process.env.BACKEND_URL || "http://localhost:3001"
+export const backendUrl = process.env.BACKEND_URL || "https://safari-books.onrender.com"
--- client/src/utils/backendURL.ts
+++ client/src/utils/backendURL.ts
@@ -1,2 +1,2 @@
-export const ipURL = "http://192.168.0.189:3001" //http + ip + port
+export const ipURL = "https://safari-books.onrender.com" //http + ip + port
 
